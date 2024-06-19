8d7705fd1a9a4e2a5deed60ef7edd6de30a5fb93
Alexander Kavanaugh <alex@kavdev.io>
2024-06-18 22:46:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
       use_scm_version={
           'write_to': 'src/dateutil/_version.py',
       },
-      version="2.8.2.dev0",
+      version="2.8.3",
       ## Needed since doctest not supported by PyPA.
       long_description = README,
       cmdclass={
